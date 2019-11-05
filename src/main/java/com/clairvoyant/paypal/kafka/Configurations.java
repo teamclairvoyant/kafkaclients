90dba95026b7dee706b0ac829bf7d57a195e3433
aravind <aravind.boppana@clairovyantsoft.com>
2019-11-05 10:35:58 -0800

    Making String Serializer as default

--- src/main/java/com/clairvoyant/paypal/kafka/Configurations.java
+++ src/main/java/com/clairvoyant/paypal/kafka/Configurations.java
@@ -2,18 +2,21 @@ package com.clairvoyant.paypal.kafka;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
 public class Configurations {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(Configurations.class);
+
 	private static Config config = ConfigFactory.load();
 
 	private static String BOOTSTRAP_SERVERS = config.getString("kafka.brokers");
 	private static String GROUP_ID = config.getString("kafka.group_id");
-	private static String KEY_DESERIALIZER = config.getString("kafka.key_deserializer");
-	private static String VALUE_DESERIALIZER = config.getString("kafka.value_deserializer");
 	private static String KAFKA_OFFSET_RESET = config.getString("kafka.auto_offset_reset");
 
 	public static Properties getProducerProperties(){
@@ -24,6 +27,9 @@ public class Configurations {
 		producerProperties.put("key.serializer", StringSerializer.class.getName());
 		producerProperties.put("value.serializer", StringSerializer.class.getName());
 
+		LOGGER.info("BOOTSTRAP_SERVERS: " + BOOTSTRAP_SERVERS);
+		LOGGER.info("GROUP_ID: " + GROUP_ID);
+
 		return producerProperties;
 	}
 
@@ -32,8 +38,12 @@ public class Configurations {
 		consumerProperties.put("bootstrap.servers", BOOTSTRAP_SERVERS);
 		consumerProperties.put("group.id", GROUP_ID);
 		consumerProperties.put("auto.offset.reset", KAFKA_OFFSET_RESET);
-		consumerProperties.put("key.deserializer", KEY_DESERIALIZER);
-		consumerProperties.put("value.deserializer", VALUE_DESERIALIZER);
+		consumerProperties.put("key.deserializer", StringDeserializer.class.getName());
+		consumerProperties.put("value.deserializer", StringDeserializer.class.getName());
+
+		LOGGER.info("BOOTSTRAP_SERVERS: " + BOOTSTRAP_SERVERS);
+		LOGGER.info("GROUP_ID: " + GROUP_ID);
+		LOGGER.info("KAFKA_OFFSET_RESET: " + KAFKA_OFFSET_RESET);
 
 		return consumerProperties;
 	}
--- src/main/java/com/clairvoyant/paypal/kafka/SimpleKafkaConsumer.java
+++ src/main/java/com/clairvoyant/paypal/kafka/SimpleKafkaConsumer.java
@@ -8,9 +8,7 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 import java.util.Collections;
-import java.util.Properties;
 
 public class SimpleKafkaConsumer {
 
